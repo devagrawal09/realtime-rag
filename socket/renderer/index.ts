6a3af93b13b2bf9e6cd9f5b5e7f02e34d2ea09df
Dev Agrawal <devagrawal09@gmail.com>
2025-01-25 04:05:21 -0600

    wip

---
+++ socket/renderer/index.ts
@@ -0,0 +1,10 @@
+import { SerializedRef } from "../lib/shared";
+
+export type Node =
+  | string
+  | {
+      type: "div" | "h1" | "button";
+      onClick?: SerializedRef;
+      style?: Record<string, string>;
+      children: Node[];
+    };
--- src/lib/counter.ts
+++ src/lib/counter.ts
@@ -2,6 +2,7 @@
 
 import { createSocketMemo } from "../../socket/lib/shared";
 import { createPersistedSignal } from "../../socket/persisted";
+import { Node } from "../../socket/renderer";
 import { storage } from "./db";
 
 const [count, setCount] = createPersistedSignal<number>(storage, `count`, 0);
@@ -10,5 +11,30 @@ export const useCounter = () => {
   const increment = () => setCount(count() + 1);
   const decrement = () => setCount(count() - 1);
 
-  return { count: createSocketMemo(count), increment, decrement };
+  return {
+    count: createSocketMemo(count),
+    increment,
+    decrement,
+    ui: createSocketMemo((): Node => {
+      return {
+        type: "div",
+        children: [
+          {
+            type: "h1",
+            children: ["Counter"],
+          },
+          {
+            type: "button",
+            style: {
+              "background-color": "yellow",
+              padding: "5px 10px",
+              border: "1px solid grey",
+              "border-radius": "5px",
+            },
+            children: [`Count ${count()}`],
+          },
+        ],
+      };
+    }),
+  };
 };
